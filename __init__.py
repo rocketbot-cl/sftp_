c3f31a6ccbe7bda327b2821cbaaa0ced297fc46e
Nico <nicogcba@gmail.com>
2022-11-07 14:53:34 -0300

    [fix] Close connection, library for Rocketbot x64

--- __init__.py
+++ __init__.py
@@ -25,11 +25,13 @@ Para instalar librerias se debe ingresar por terminal a la carpeta "libs"
 """
 
 import os
-import sys
+
 base_path = tmp_global_obj["basepath"]
 cur_path = base_path + 'modules' + os.sep + 'sftp_' + os.sep + 'libs' + os.sep
-# print(cur_path)
-sys.path.append(cur_path)
+
+cur_path_x64 = os.path.join(cur_path, 'Windows' + os.sep +  'x64' + os.sep)
+cur_path_x86 = os.path.join(cur_path, 'Windows' + os.sep +  'x86' + os.sep)
+
 
 import pysftp
 """
@@ -43,207 +45,217 @@ global serverOs
 
 module = GetParams("module")
 
-if module == "conn_sftp":
+try:
+    if module == "conn_sftp":
 
-    server_ = GetParams("server_")
-    user_ = GetParams("user_")
-    pass_ = GetParams("pass_")
-    port_ = GetParams("port_")
-    serverOs = GetParams("serverOs")
+        server_ = GetParams("server_")
+        user_ = GetParams("user_")
+        pass_ = GetParams("pass_")
+        port_ = GetParams("port_")
+        serverOs = GetParams("serverOs")
 
-    if serverOs == "Windows":
-        separator = "\\"
-    
-    if not port_:
-        port_ = 22
-    else:
-        port_ = int(port_)
-    pem = GetParams("pem")
-    var_ = GetParams("var_")
-
-    try:
-        if pem:
-            pconn = pysftp.Connection(
-                host=server_,
-                username=user_,
-                private_key=pem,
-            )
-            print("Connection succesfully stablished ... ")
+        if serverOs == "Windows":
+            separator = "\\"
+        
+        if not port_:
+            port_ = 22
         else:
-            cnopts = pysftp.CnOpts()
-            cnopts.hostkeys = None
-            pconn = pysftp.Connection(host=server_, username=user_, password=pass_, port=port_, cnopts=cnopts)
-            print("Connection succesfully stablished ... ")
-        res = True
-    except:
-        PrintException()
-        res = False
-
-    SetVar(var_, res)
-
-if module == "go_dir":
-
-    try:
-        dir_ = GetParams("dir_")
+            port_ = int(port_)
+        pem = GetParams("pem")
         var_ = GetParams("var_")
 
-        go_ = pconn.cwd(dir_)
-        pwd_ = pconn.pwd
-
-        SetVar(var_,pwd_)
-
-    except Exception as e:
-        PrintException()
-        raise e
-
-if module == "upload_":
-
-    
-
-    file_ = GetParams("file_")
-    var_ = GetParams("var_")
-
-    dir_ = GetParams("dir_")
-
-    try:
-        current = pconn.chdir('.')
-        current = pconn.getcwd()
-        filename = os.path.basename(file_)
-        if not dir_:
-#            pconn.put(file_, os.path.join(current, filename))
-            finalPathServer = os.path.join(current, filename)
-            if serverOs == "Windows":
-                finalPathServer = finalPathServer.replace("/", "\\")
-            else:
-                finalPathServer = finalPathServer.replace("\\", "/")
-            pconn.put(file_, finalPathServer)
-        else:
-            finalPathServer = os.path.join(dir_, filename)
-            if serverOs == "Windows":
-                finalPathServer = finalPathServer.replace("/", "\\")
-            else:
-                finalPathServer = finalPathServer.replace("\\", "/")
-            pconn.put(file_, finalPathServer)
-        res = True
-
-    except:
-        PrintException()
-        res = False
-
-    SetVar(var_, res)
-
-if module == "download_":
-
-    file_ = GetParams("file_")
-    path_ = GetParams("path_")
-    var_ = GetParams("var_")
-
-    try:
-        current = pconn.chdir('.')
-        current = pconn.getcwd()
-        pwd_ = ""
-        if pwd_ == "":
-            pwd_ = current
-        
-        # Define the file that you want to download from the remote directory
-        remoteFilePath = os.path.join(pwd_,file_)
-        if serverOs == "Windows":
-            remoteFilePath = remoteFilePath.replace("/", "\\")
-        else:
-            remoteFilePath = remoteFilePath.replace("\\", "/")
-        filename = os.path.basename(remoteFilePath)
-
-        # Define the local path where the file will be saved
-        # or absolute "C:\Users\sdkca\Desktop\TUTORIAL.txt"
-        localFilePath = path_
-
-
-        localFilePath = os.path.join(localFilePath, filename)
-        if os.path == "/":
-            localFilePath = localFilePath.replace("/", "\\")
-        else:
-            localFilePath = localFilePath.replace("\\", "/")
-
-        pconn.get(remoteFilePath, localFilePath)
-        res = True
-    except:
-        PrintException()
         try:
-            os.remove(localFilePath)
+            if pem:
+                pconn = pysftp.Connection(
+                    host=server_,
+                    username=user_,
+                    private_key=pem,
+                )
+                print("Connection succesfully stablished ... ")
+            else:
+                cnopts = pysftp.CnOpts()
+                cnopts.hostkeys = None
+                pconn = pysftp.Connection(host=server_, username=user_, password=pass_, port=port_, cnopts=cnopts)
+                print("Connection succesfully stablished ... ")
+            res = True
         except:
-            pass
-        res = False
+            PrintException()
+            res = False
 
-    SetVar(var_, res)
+        SetVar(var_, res)
 
-if module == "delete_file":
-    file_ = GetParams("file_")
-    var_ = GetParams("var_")
+    if module == "go_dir":
 
-    try:
-        delFilePath = os.path.join(pwd_, file_)
-        if serverOs == "Windows":
-            delFilePath = delFilePath.replace("/", "\\")
-        else:
-            delFilePath = delFilePath.replace("\\", "/")
-        del_ = pconn.remove(delFilePath)
-        res = True
+        try:
+            dir_ = GetParams("dir_")
+            var_ = GetParams("var_")
 
-    except:
-        PrintException()
-        res = False
+            go_ = pconn.cwd(dir_)
+            pwd_ = pconn.pwd
 
-    SetVar(var_, res)
+            SetVar(var_,pwd_)
 
-if module == "list_files":
+        except Exception as e:
+            PrintException()
+            raise e
 
-    try:
-        dir_ = GetParams("dir_")
-        var_ = GetParams("var_")
+    if module == "upload_":
 
-        listedFiles = pconn.listdir(dir_)
+        
 
-        SetVar(var_, listedFiles)
-
-    except Exception as e:
-        PrintException()
-        raise e
-    
-if module == "rename_file":
-
-    try:
         file_ = GetParams("file_")
-        new_name = GetParams("new_name")
         var_ = GetParams("var_")
 
-        oldPath = os.path.join(pwd_, file_)
-        
-        if serverOs == "Windows":
-            oldPath = oldPath.replace("/", "\\")
-        else:
-            oldPath = oldPath.replace("\\", "/")
-        newPath = os.path.join(pwd_, new_name)
-        if serverOs == "Windows":
-            newPath = newPath.replace("/", "\\")
-        else:
-            newPath = newPath.replace("\\", "/")
-        pconn.rename(oldPath, newPath)
-        res = True
+        dir_ = GetParams("dir_")
 
-    except Exception as e:
-        PrintException()
-        res = False
+        try:
+            current = pconn.chdir('.')
+            current = pconn.getcwd()
+            filename = os.path.basename(file_)
+            if not dir_:
+    #            pconn.put(file_, os.path.join(current, filename))
+                finalPathServer = os.path.join(current, filename)
+                if serverOs == "Windows":
+                    finalPathServer = finalPathServer.replace("/", "\\")
+                else:
+                    finalPathServer = finalPathServer.replace("\\", "/")
+                pconn.put(file_, finalPathServer)
+            else:
+                finalPathServer = os.path.join(dir_, filename)
+                if serverOs == "Windows":
+                    finalPathServer = finalPathServer.replace("/", "\\")
+                else:
+                    finalPathServer = finalPathServer.replace("\\", "/")
+                pconn.put(file_, finalPathServer)
+            res = True
 
-    SetVar(var_, res)
-    
-if module == "close_":
-    var_ = GetParams("var_")
-    
-    try:
-        pconn.close()
-        res = True
-    except Exception as e:
-        PrintException()
-        res = False
+        except:
+            PrintException()
+            res = False
+
+        SetVar(var_, res)
+
+    if module == "download_":
+
+        file_ = GetParams("file_")
+        path_ = GetParams("path_")
+        var_ = GetParams("var_")
+
+        try:
+            current = pconn.chdir('.')
+            current = pconn.getcwd()
+            pwd_ = ""
+            if pwd_ == "":
+                pwd_ = current
+            
+            # Define the file that you want to download from the remote directory
+            remoteFilePath = os.path.join(pwd_,file_)
+            if serverOs == "Windows":
+                remoteFilePath = remoteFilePath.replace("/", "\\")
+            else:
+                remoteFilePath = remoteFilePath.replace("\\", "/")
+            filename = os.path.basename(remoteFilePath)
+
+            # Define the local path where the file will be saved
+            # or absolute "C:\Users\sdkca\Desktop\TUTORIAL.txt"
+            localFilePath = path_
+
+
+            localFilePath = os.path.join(localFilePath, filename)
+            if os.path == "/":
+                localFilePath = localFilePath.replace("/", "\\")
+            else:
+                localFilePath = localFilePath.replace("\\", "/")
+
+            pconn.get(remoteFilePath, localFilePath)
+            res = True
+        except:
+            PrintException()
+            try:
+                os.remove(localFilePath)
+            except:
+                pass
+            res = False
+
+        SetVar(var_, res)
+
+    if module == "delete_file":
+        file_ = GetParams("file_")
+        var_ = GetParams("var_")
+
+        try:
+            delFilePath = os.path.join(pwd_, file_)
+            if serverOs == "Windows":
+                delFilePath = delFilePath.replace("/", "\\")
+            else:
+                delFilePath = delFilePath.replace("\\", "/")
+            del_ = pconn.remove(delFilePath)
+            res = True
+
+        except:
+            PrintException()
+            res = False
+
+        SetVar(var_, res)
+
+    if module == "list_files":
+
+        try:
+            dir_ = GetParams("dir_")
+            var_ = GetParams("var_")
+
+            listedFiles = pconn.listdir(dir_)
+
+            SetVar(var_, listedFiles)
+
+        except Exception as e:
+            PrintException()
+            raise e
         
-    SetVar(var_, res)
+    if module == "rename_file":
+
+        try:
+            file_ = GetParams("file_")
+            new_name = GetParams("new_name")
+            var_ = GetParams("var_")
+
+            oldPath = os.path.join(pwd_, file_)
+            
+            if serverOs == "Windows":
+                oldPath = oldPath.replace("/", "\\")
+            else:
+                oldPath = oldPath.replace("\\", "/")
+            newPath = os.path.join(pwd_, new_name)
+            if serverOs == "Windows":
+                newPath = newPath.replace("/", "\\")
+            else:
+                newPath = newPath.replace("\\", "/")
+            pconn.rename(oldPath, newPath)
+            res = True
+
+        except Exception as e:
+            PrintException()
+            res = False
+
+        SetVar(var_, res)
+        
+    if module == "close_":
+        var_ = GetParams("var_")
+        
+        try:
+            if pconn:
+                pconn.close()
+                pconn = None
+                SetVar(var_, True)
+            else:
+                res = False
+                raise Exception("Connection not stablished")
+        except Exception as e:
+            PrintException()
+            SetVar(var_, False)
+            raise e
+            
+        
+except Exception as e:
+    PrintException()
+    raise e
